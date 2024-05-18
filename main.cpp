83f392f7b491911e913c6dcffcd0cf8bd36aa97a
ForgeDom <maxstill2006@gmail.com>
2024-05-18 15:49:43 +0300

    Lab ex_2 done

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 #include <Windows.h>
 using namespace std;
 
-main() {
+int main() {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
 
+	const int arr_size = 5;
+	int arr[arr_size] = { 1,2,3,4,5 };
+	for (int i = arr_size - 1; i >= 0; i--) {
+		cout << arr[i] << " ";
+	}
 	return 0;
 }
