24c4587223ddb3db9b19036d52b3d4a18cbc9c74
ForgeDom <maxstill2006@gmail.com>
2024-05-18 16:19:42 +0300

    Hm ex 2 done

--- main.cpp
+++ main.cpp
@@ -3,8 +3,40 @@
 using namespace std;
 
 int main() {
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
 
-	return 0;
+    const int size = 12;
+    int arr[size] = {};
+    int sal, min, max, dia1, dia2;
+
+    for (int i = 0; i < size; i++) {
+        cin >> sal;
+        arr[i] = sal;
+    }
+
+    cout << "Введіть діапазон в межах від 0 до 11: ";
+    cin >> dia1 >> dia2;
+
+    if (dia1 < 0 || dia1 >= size || dia2 < 0 || dia2 >= size || dia1 > dia2) {
+        cout << "Неправильний діапазон" << endl;
+        return 1;
+    }
+
+    min = arr[dia1];
+    max = arr[dia1];
+
+    for (int i = dia1 + 1; i <= dia2; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    cout << "Мінімальне значення в діапазоні: " << min << endl;
+    cout << "Максимальне значення в діапазоні: " << max << endl;
+
+    return 0;
 }
