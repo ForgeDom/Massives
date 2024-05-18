494f5ef70292b30be31806d2969dd8b69e5ba534
ForgeDom <maxstill2006@gmail.com>
2024-05-18 16:06:13 +0300

    Lab ex4 done

--- main.cpp
+++ main.cpp
@@ -2,9 +2,33 @@
 #include <Windows.h>
 using namespace std;
 
-main() {
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
 
-	return 0;
+    const int size = 12;
+    int arr[size] = {};
+    int sal, min, max;
+
+    for (int i = 0; i < size; i++) {
+        cin >> sal;
+        arr[i] = sal;
+    }
+
+    min = arr[0];
+    max = arr[0];
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    cout << "Мінімальне значення: " << min << endl;
+    cout << "Максимальне значення: " << max << endl;
+
+    return 0;
 }
