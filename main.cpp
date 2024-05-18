00485924ac69cc63bb0b04dce8d2c590ff448945
ForgeDom <maxstill2006@gmail.com>
2024-05-18 15:56:05 +0300

    Lab ex3 done

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 #include <Windows.h>
 using namespace std;
 
-main() {
+int main() {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
 
+	const int size = 5;
+	int arr[size] = {};
+	int elem, sum = 0;
+	for (int i = 0; i < size; i++) {
+		cin >> elem;
+		arr[i] = elem;
+	}
+	for (int i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+	cout << sum;
 	return 0;
 }
