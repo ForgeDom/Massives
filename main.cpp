ef20b7c0266bad95ee52e7d5a527da537b58df2b
ForgeDom <maxstill2006@gmail.com>
2024-05-18 17:13:42 +0300

    ex done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
-#include <Windows.h>
 using namespace std;
 
-main() {
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
-
-	return 0;
+int main()
+{
+    const int size = 8;
+    double arr[size] = { -5.7, 6.0, 2, 0, -4.7, 6, 8.1, -4 };
+    double positive = 0;
+    for (int i = size - 1; i >= 0; i--)
+    {
+        if (arr[i] > 0)
+        {
+            positive = arr[i];
+            break;
+        }
+    }
+    double negative = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < 0)
+        {
+            negative = arr[i];
+            break;
+        }
+    }
+    cout << "Last positive number: " << positive << endl;
+    cout << "First negative number: " << negative << endl;
+    return 0;
 }
