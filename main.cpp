9e0b565cc2387bec4eae778034d77772436fc8e1
ForgeDom <maxstill2006@gmail.com>
2024-05-18 15:44:51 +0300

    Lab ex1 done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <Windows.h>
+#include <ctime>
 using namespace std;
 
-main() {
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    srand(time(0));
+    int salary;
+    const int max_size = 6;
+    int arr[max_size] = { 0 };
+    int sum = 0;
 
-	return 0;
+    for (int i = 0; i < max_size; i++) {
+        cin >> salary;
+        arr[i] = salary;
+    }
+
+    for (int i = 0; i < max_size; i++) {
+        sum += arr[i];
+    }
+    cout << sum;
+    return 0;
 }
